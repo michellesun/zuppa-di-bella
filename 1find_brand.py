42fa31f50c83192bc271131291247983194427c8
Michelle <michesun@gmail.com>
2012-09-22 14:00:43 +0800

    pylinting find_x files

--- 1find_brand.py
+++ 1find_brand.py
@@ -1,186 +1,151 @@
 from bs4 import BeautifulSoup
 import requests
-from collections import Counter
-from collections import Counter, defaultdict
-from operator import itemgetter
+from collections import defaultdict
 import pymongo
 from sets import Set
 
 def make_board_urllist(brand):
 ### make a list of first 5 pages of URLs based on brand search
-	bs = brand.split()
-	if len(bs) > 1:
-		name = "+".join(bs)
-		brand_url = "http://pinterest.com/search/?q=%s" %(name)
-	else:
-		brand_url = "http://pinterest.com/search/?q=%s" %(brand)
-	url_list = []
-	for i in range(1,11): #get the first 10 pages of results
-		new_url = ( brand_url+"&page=%d") %i 
-		url_list.append(new_url) 
-	"""print "url_list", url_list
-
-		url_list ['http://pinterest.com/search/?q=Cobra+Society&page=1', 'http://pinterest.com/search/?q=Cobra+Society&page=2', 'http://pinterest.com/search/?q=Cobra+Society&page=3', 'http://pinterest.com/search/?q=Cobra+Society&page=4', 'http://pinterest.com/search/?q=Cobra+Society&page=5', 'http://pinterest.com/search/?q=Cobra+Society&page=6', 'http://pinterest.com/search/?q=Cobra+Society&page=7', 'http://pinterest.com/search/?q=Cobra+Society&page=8', 'http://pinterest.com/search/?q=Cobra+Society&page=9', 'http://pinterest.com/search/?q=Cobra+Society&page=10']
-	"""
-	return url_list
+    bs = brand.split()
+    if len(bs) > 1:
+        name = "+".join(bs)
+        brand_url = "http://pinterest.com/search/?q=%s" %(name)
+    else:
+        brand_url = "http://pinterest.com/search/?q=%s" %(brand)
+    url_list = []
+    for i in range(1, 11): # get the first 10 pages of results
+        new_url = ( brand_url+"&page=%d") %i 
+        url_list.append(new_url) 
+    """print "url_list", url_list
+        url_list ['http://pinterest.com/search/?q=Cobra+Society&page=1', 'http://pinterest.com/search/?q=Cobra+Society&page=2', 'http://pinterest.com/search/?q=Cobra+Society&page=3', 'http://pinterest.com/search/?q=Cobra+Society&page=4', 'http://pinterest.com/search/?q=Cobra+Society&page=5', 'http://pinterest.com/search/?q=Cobra+Society&page=6', 'http://pinterest.com/search/?q=Cobra+Society&page=7', 'http://pinterest.com/search/?q=Cobra+Society&page=8', 'http://pinterest.com/search/?q=Cobra+Society&page=9', 'http://pinterest.com/search/?q=Cobra+Society&page=10']
+    """
+    return url_list
 
 def make_user_list(url_list):
-	user_list = Set()
-	for url in url_list:
-		try:
-			page = requests.get(url)
-			pinsoup = BeautifulSoup(page.content)
-			pinners = get_pinners(pinsoup)
-			for pinner in pinners:
-				user_list.add(pinner)
-		except Exception,e:
-			pass
-	print "len(user_list)", len(user_list) # 19360
-	return user_list #list of all users that have pinned in these brands
+## make a list of all users that have pinned in these brands
+## include duplicates
+    user_list = Set()
+    for url in url_list:
+        try:
+            page = requests.get(url)
+            pinsoup = BeautifulSoup(page.content)
+            pinners = get_pinners(pinsoup)
+            for pinner in pinners:
+                user_list.add(pinner)
+        except Exception, e:
+            pass
+    return user_list # 19360
 
 ## new function!! ## 
 def checkoverlap(url_list):
-	check_overlap = []
-
-	for url in url_list:
-		try:
-			page = requests.get(url)
-			pinsoup = BeautifulSoup(page.content)
-			pinners = get_pinners(pinsoup)
-			print pinners
-			check_overlap + pinners
-		except Exception,e:
-			pass
-	return check_overlap
+    check_overlap = []
+    for url in url_list:
+        try:
+            page = requests.get(url)
+            pinsoup = BeautifulSoup(page.content)
+            pinners = get_pinners(pinsoup)
+            print pinners
+            check_overlap + pinners
+        except Exception, e:
+            pass
+    return check_overlap
 
 def make_indieuser_list(url_list): 
 ### now having the list of url, get list of users that pinned those pins
-	user_list = []
-	i = 0
-	for url in url_list:
-		each_list = []
-		try:
-			page = requests.get(url)
-			pinsoup = BeautifulSoup(page.content)
-			pinners = get_pinners(pinsoup)
-			# print "url", url
-			# print "pinners", pinners 
-			for pinner in pinners:
-				# print "pinner", pinner
-				each_list.append(pinner)
-		except Exception,e:
-			pass
-		i += 1
-		# print "each_list", i, each_list	
-		user_list.extend(each_list)	
-	
-	print "len of user list", url , len(user_list) # Derek Lam 463
-	return user_list #list of users that pinned in 1 brand (with duplicated) around 450
+    user_list = []
+    i = 0
+    for url in url_list:
+        each_list = []
+        try:
+            page = requests.get(url)
+            pinsoup = BeautifulSoup(page.content)
+            pinners = get_pinners(pinsoup)
+            for pinner in pinners:
+                each_list.append(pinner)
+        except Exception, e:
+            pass
+        i += 1
+        user_list.extend(each_list) 
+    # list of users that pinned in 1 brand (with duplicated) around 450 
+    return user_list 
 
 def get_pinners(pinsoup):
 ### find sources of pins
-	p = pinsoup.find_all('div', {'class': 'convo attribution clearfix'})
-	# <a href="/ivanaamai/" title="Ivana Amai" class="ImgLink">
-	"""	an item in p
-		<div class="convo attribution clearfix">
-		<a class="ImgLink" href="/rebecasantanna/" title="Rebeca Sant'Anna">
-		<img alt="Profile picture of Rebeca Sant'Anna" src="http://media-cache-ec6.pinterest.com/avatars/rebecasantanna_1337063676.jpg"/>
-		</a>
-		<p>
-		<a href="/rebecasantanna/">Rebeca Sant'Anna</a> onto <a href="/rebecasantanna/favorites/">Favorites</a>
-		</p>
-		</div>
+    p = pinsoup.find_all('div', {'class': 'convo attribution clearfix'})
+    # <a href="/ivanaamai/" title="Ivana Amai" class="ImgLink">
+    """ an item in p
+        <div class="convo attribution clearfix">
+        <a class="ImgLink" href="/rebecasantanna/" title="Rebeca Sant'Anna">
+        <img alt="Profile picture of Rebeca Sant'Anna" src="http://media-cache-ec6.pinterest.com/avatars/rebecasantanna_1337063676.jpg"/>
+        </a>
+        <p>
+        <a href="/rebecasantanna/">Rebeca Sant'Anna</a> onto <a href="/rebecasantanna/favorites/">Favorites</a>
+        </p>
+        </div>
     """
-	pinners = []
-
-	for item in p:
-		pinner = item.contents[1]['href'][1:-2] #how do I access href??
-		""" pinner
-			'staciarose2'
-			'christopherjo'
-			'andreascarlsso'
-			'faragodesig'
-			'mistressnatali'
-			'danielplanvie'
-			'rebecasantann'
-		"""
-		pinners.append(pinner)
-	# print 'pinners', pinners
-	"""pinners
-		pinners ['brooke_taylor1', 'aidinbelgane', 'lzampell', 'hypergenica1', 'debroahreze', 'lauramas', 'rast', 'snowpeaprinces', 'carolinebu', 'marycomett', 'tenditrend', 'magpieluc', 'uniquewa', 'grizzly112', 'kcopacinn', 'johannsenphot', 'palom', 'christineo201', 'jerseegir', 'marymichelott', 'ttownrussia', 'bridgetttaa', 'saar', 'kelkel101', 'born2boo', 'wlsca', 'kyladoyl', 'naebearb', 'angelada', 'riverrunn', 'lilgreylex']
-		pinners ['wholeean', 'ayoung1', 'meev', 'sarahblat', 'sam', 'charlilo', 'suburban2', 'carmenstreete', 'carabmisle', 'efenhau', 'angelemateu', 'teogavala', 'tammybernin', 'eviasikarsk', 'pfredericka', 'starphia', 'akfranke', 'katieottewel', 'zizzie7', 'shelleyraymond', 'onyxroz', 'eleanorpasche6', 'gchga', 'brooke_taylor1', 'aidinbelgane', 'lzampell', 'hypergenica1', 'debroahreze', 'lauramas', 'rast', 'snowpeaprinces', 'carolinebu', 'marycomett']
-	"""
-	return pinners
-
+    pinners = []
+    for item in p:
+        pinner = item.contents[1]['href'][1:-2] 
+        """ pinner
+            'staciarose2'
+            'christopherjo'
+        """
+        pinners.append(pinner)
+    """ pinners
+        pinners ['brooke_taylor1', 'aidinbelgane', 'lzampell', 'hypergenica1', 'debroahreze', 'lauramas', 'rast', 'snowpeaprinces', 'carolinebu', 'marycomett', 'tenditrend', 'magpieluc', 'uniquewa', 'grizzly112', 'kcopacinn', 'johannsenphot', 'palom', 'christineo201', 'jerseegir', 'marymichelott', 'ttownrussia', 'bridgetttaa', 'saar', 'kelkel101', 'born2boo', 'wlsca', 'kyladoyl', 'naebearb', 'angelada', 'riverrunn', 'lilgreylex']
+        pinners ['wholeean', 'ayoung1', 'meev', 'sarahblat', 'sam', 'charlilo', 'suburban2', 'carmenstreete', 'carabmisle', 'efenhau', 'angelemateu', 'teogavala', 'tammybernin', 'eviasikarsk', 'pfredericka', 'starphia', 'akfranke', 'katieottewel', 'zizzie7', 'shelleyraymond', 'onyxroz', 'eleanorpasche6', 'gchga', 'brooke_taylor1', 'aidinbelgane', 'lzampell', 'hypergenica1', 'debroahreze', 'lauramas', 'rast', 'snowpeaprinces', 'carolinebu', 'marycomett']
+    """
+    return pinners
 
 def count_source(per_brand_user_list):
 ### feed all sources and generate a list of tuples with (count, domain)
-	output = defaultdict(lambda: 0)
-	for user in per_brand_user_list:
-	# because there are duplicates
-		output[user] += 1
-
- 	# source_count = sorted(output.iteritems(),key=lambda (k,v): v,reverse=True)		
-	# user_count = sorted(output.iteritems(),key=lambda (k,v): v,reverse=True)
-	# print "source_count", sorted(output.iteritems(),key=lambda (k,v): v,reverse=True)
-	return output
+    output = defaultdict(lambda: 0)
+    for user in per_brand_user_list:
+        output[user] += 1
+    return output
 
 def connect_db():
-	connect_string = "mongodb://pinterestalgo:pinterest2012@ds037047.mongolab.com:37047/brands"
-	# mongodb://%s:%s@%s:%d/%s" % \
- #            (user, password, host, port, db_name)
-	c = pymongo.connection.Connection(connect_string)
-	return c['brands']
+    connect_string = "mongodb://X:X@X.mongolab.com:X/X"
+    # mongodb://%s:%s@%s:%d/%s" % \
+    #   (user, password, host, port, db_name)
+    c = pymongo.connection.Connection(connect_string)
+    return c['brands']
 
 def main():
-	global db
-	db = connect_db()
-	# db = db['brand'] #get collection named pinterest
-	t = open("/Users/honeysnow/Desktop/python/beautifulsoup/datafiles/topbrands_shopbop2.txt")
-	# url_list = []
-	for line in t.readlines():
-		brand = line.strip()
-		all_pinners_list = []	
-		url = make_board_urllist(brand)
-		# url_list.extend(url) 
-		# """url_list a list of urls for all brands, all pages
+    global db
+    db = connect_db()
+    # db = db['brand'] #get collection named pinterest
+    t = open("/Users/honeysnow/Desktop/python/beautifulsoup/datafiles/topbrands_shopbop2.txt")
+    # url_list = []
+    for line in t.readlines():
+        brand = line.strip()
+        all_pinners_list = []   
+        url_list = make_board_urllist(brand)
+        per_brand_user_list = make_indieuser_list(url_list)
+        # print "per_brand_user_list", brand, per_brand_user_list
+        output = count_source(per_brand_user_list)
+        # print 'output', output
+        # print 'len(output), size of dictionary', len(output)
+        output_dict = {}
+        output_dict['brand'] = brand
+        output_dict['users'] = output
 
-		# 	 'http://pinterest.com/search/?q=seven-jeans&page=0', 'http://pinterest.com/search/?q=seven-jeans&page=1', 'http://pinterest.com/search/?q=seven-jeans&page=2', 'http://pinterest.com/search/?q=seven-jeans&page=3', 'http://pinterest.com/search/?q=seven-jeans&page=4', 'http://pinterest.com/search/?q=seven-jeans&page=5', 'http://pinterest.com/search/?q=tom-ford&page=0', 'http://pinterest.com/search/?q=tom-ford&page=1', 'http://pinterest.com/search/?q=tom-ford&page=2', 'http://pinterest.com/search/?q=tom-ford&page=3', 'http://pinterest.com/search/?q=tom-ford&page=4', 'http://pinterest.com/search/?q=tom-ford&page=5']
-		# """
-		# unindent the following 5 lines
-		# user_list = make_user_list(url_list) #make a large SET userlist that includes all users that pinned from all brands all pages ## What is this for???
+        # logging
+        # log_dict_len(brand, len(output_dict['users']))
+        # log_empty(output_dict)
+        # keep track of length of each dictionary
+        lenlog = open('datafiles/length.txt','a')
+        lenlog.write("%s\t%d\n" % (brand, len(output_dict['users'])))
+        lenlog.close()
 
-		# t = open("/Users/honeysnow/Desktop/python/beautifulsoup/datafiles/topbrands_shopbop.txt")
-		# for line in t.readlines():
-		# 	# now get a list of users( with duplicates ) 
-		# 	brand = line.strip()
-		# no need for now
+        # keep track of empty returns and rerun. 
+        if len(output_dict) == 0:
+            f = open('datafiles/emptybrand.txt', 'w')
+            f.write(brand)
+            f.close()
 
-		url_list = make_board_urllist(brand)
-		per_brand_user_list = make_indieuser_list(url_list)
-		# print "per_brand_user_list", brand, per_brand_user_list
-		output = count_source(per_brand_user_list)
-		# print 'output', output
-		# print 'len(output), size of dictionary', len(output)
-		output_dict = {}
-		output_dict['brand'] = brand
-		output_dict['users'] = output
-		# print 'output_dict', output_dict
-
-		lenlog = open('datafiles/length.txt','a')
-		lenlog.write("%s\t%d\n" % (brand,len(output_dict['users'])))
-		lenlog.close()
-		# keep track of length of each dictionary
-
-		if len(output_dict) == 0:
-			f = open('datafiles/emptybrand.txt','w')
-			f.write(brand)
-			f.close()
-			# keep track of empty returns and rerun. 
-
-
-		brands = db.brands
-		brands.insert(output_dict)
-	
+        brands = db.brands
+        brands.insert(output_dict)
+    
 if __name__ == '__main__':
-	main()
+    main()
--- 1find_pin.py
+++ 1find_pin.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-from collections import Counter
-from collections import Counter, defaultdict
-from operator import itemgetter
+from collections import defaultdict
 import pymongo
 
 #toppinners3.txt should start with
@@ -10,229 +8,174 @@ import pymongo
 
 def find_board_list(username):
 ### find a list of boards by inserting username
-	user_url = "http://pinterest.com/%s" %(username)
-	u = requests.get(user_url)
-	user_soup = BeautifulSoup(u.content)
-	user_boards = user_soup.find_all("div", "pin pinBoard")
-	board_list = []
-	i = 0
-	for item in user_boards:
-		if user_boards[i] == None:
-			continue
-		else:
-			name = user_boards[i].contents[1].contents[0].text
-			board_list.append(name)
-		i += 1
-	return board_list
+    user_url = "http://pinterest.com/%s" %(username)
+    u = requests.get(user_url)
+    user_soup = BeautifulSoup(u.content)
+    user_boards = user_soup.find_all("div", "pin pinBoard")
+    board_list = []
+    i = 0
+    for item in user_boards:
+        if user_boards[i] == None:
+            continue
+        else:
+            name = user_boards[i].contents[1].contents[0].text
+            board_list.append(name)
+        i += 1
+    return board_list
 
 def transform_boardname(board_list):
 ### change names of boards with more than 1 word to link up with hyphens
 ### eg, "home sweet home" to "home-sweet-home"
-	renamed_list = []
-	for name in board_list:
-		namelist = name.split()
-		if len(namelist) > 1:
-			name = '-'.join(namelist)
-			renamed_list.append(name)
-		else:
-			renamed_list.append(name)
-	return renamed_list
-	""" board_list
-			[u'fitness', u'beauty', u'home sweet home', u'words', u'sweetness', 
-			u'wanderlust', u'fooodism', u'infographics', u'items', u'nature', 
-			u'love in the bay', u'web design', u'books ', u'demo videos']
-	"""
-	""" renamed_board
-			[u'fitness', u'beauty', u'home-sweet-home', u'words', u'sweetness', 
-			u'wanderlust', u'fooodism', u'infographics', u'items', u'nature', 
-			u'love-in-the-bay', u'web-design', u'books ', u'demo-videos']
-	"""
+    renamed_list = []
+    for name in board_list:
+        namelist = name.split()
+        if len(namelist) > 1:
+            name = '-'.join(namelist)
+            renamed_list.append(name)
+        else:
+            renamed_list.append(name)
+    return renamed_list
 
 def find_board_url(username, boardname):
-	pboard_url = "http://pinterest.com/%s/%s/" %(username,boardname)
-	pinb = requests.get(pboard_url)
-	pinsoup = BeautifulSoup(pinb.content)
-	pin_num_div = pinsoup.find("div",{"id":"BoardStats"})
-	try: 
-		pin_num = pin_num_div.contents[3].contents[0]
-		url_list = make_url_list(pin_num,pboard_url)
-		return url_list
-	except Exception,e: 
-		pass #or print error
-### find url_list (page 1-2) by injecting boardname
-"""url list
-	['http://pinterest.com/michellelsun/beauty', 
-	'http://pinterest.com/michellelsun/beauty?page=2', 
-	'http://pinterest.com/michellelsun/beauty?page=3']
-"""
+## construct url list based on how many pins there are
+## eg, if 'beauty' board has 100 pins,
+## make a list of [... "http://pinterest.com/michellelsun/beauty?page=3']"
+    pboard_url = "http://pinterest.com/%s/%s/" %(username,boardname)
+    pinb = requests.get(pboard_url)
+    pinsoup = BeautifulSoup(pinb.content)
+    pin_num_div = pinsoup.find("div", {"id":"BoardStats"})
+    try: 
+        pin_num = pin_num_div.contents[3].contents[0]
+        url_list = make_url_list(pin_num, pboard_url)
+        return url_list
+    except Exception, e: 
+        pass 
+
+def make_url_list(pin_num, pboard_url):
+### make URL in infinite scrolling 
+### http://pinterest.com/michellelsun/beauty/?page=3
+    if int(pin_num) % 50 != 0:
+        page_num = int(pin_num) / 50 + 1
+    else:
+        page_num = int(pin_num) / 50
+    url_list = [pboard_url]
+    i = 2
+    while i <= page_num:
+        new_url = ( pboard_url+"?page=%d") %i
+        url_list.append(new_url) 
+        i += 1
+    return url_list
 
 def make_pin_source_list(url_list):
 ### now having the list of board url, get list of pins
-	pin_source_list = []
-	for url in url_list:
-		try:
-			new_pinb = requests.get(url)
-			pinsoup = BeautifulSoup(new_pinb.content)
-			pin_source = get_pin_source(pinsoup)
-			pin_source_list.extend(pin_source)
-		except Exception,e:
-			pass
-	# print pin_source_list, len(pin_source_list)
-	return pin_source_list
-
-def make_url_list(pin_num,pboard_url):
-### make URL in infinite scrolling 
-### http://pinterest.com/michellelsun/beauty/?page=3
-	if int(pin_num) % 50 != 0:
-		page_num = int(pin_num) / 50 + 1
-	else:
-		page_num = int(pin_num) / 50
-	url_list = [pboard_url]
-	i = 2
-	while i <= page_num:
-		new_url = ( pboard_url+"?page=%d") %i
-		url_list.append(new_url) 
-		i += 1
-	return url_list
+    pin_source_list = []
+    for url in url_list:
+        try:
+            new_pinb = requests.get(url)
+            pinsoup = BeautifulSoup(new_pinb.content)
+            pin_source = get_pin_source(pinsoup)
+            pin_source_list.extend(pin_source)
+        except Exception, e:
+            pass
+    return pin_source_list
 
 def get_pin_source(pinsoup):
 ### find sources of pins
-	all_linked_pins = pinsoup.find_all("div","convo attribution clearfix")
-	"""	an item in all_linked_pins
-		<div class="convo attribution clearfix">
-		<p class="NoImage">
-		<a href="http://weheartit.com/entry/23003667" rel="nofollow" target="_blank">weheartit.com</a>
-		</p>
-		</div>
-	"""
-	pin_source = []
-	""" error message 
-		<type 'NoneType'>
-		<class 'bs4.element.Tag'>
-		<class 'bs4.element.Tag'>
-		<class 'bs4.element.Tag'>
-		<class 'bs4.element.Tag'>
-		<class 'bs4.element.Tag'>
-		<class 'bs4.element.Tag'>
-		<class 'bs4.element.Tag'>
-		<type 'NoneType'>
-	"""
-	# i = 0
-	for item in all_linked_pins:
-	 	if item.a == None:
-			continue
-		else:
-			pin_source.append(item.a.contents[0])
-		# i += 1
-	return pin_source
-	""" sample pin_source
-		[u'thefancy.com', u'raspberryandred.blogspot.com', 
-		u'rstyle.me', u'25.media.tumblr.com', u'mostbeautifull.net', 
-		u'threadflip.com', u'rstyle.me', u'kingsunderlavenderskies.tumblr.com', 
-		u'chrisanthemums.tumblr.com', u'laurenconrad.com', 
-		u'whyilovetoshop.com', u'iknowhair.com', u'rstyle.me',
-		 u'shopbop.com', u'community.boden.co.uk', u'zimbio.com', 
-		 u'glamour.com', u'themessesofmen.tumblr.com']
-	"""
+    all_linked_pins = pinsoup.find_all("div","convo attribution clearfix")
+    """ an item in all_linked_pins
+        <div class="convo attribution clearfix">
+        <p class="NoImage">
+        <a href="http://weheartit.com/entry/23003667" rel="nofollow" \
+        target="_blank">weheartit.com</a>
+        </p>
+        </div>
+    """
+    pin_source = []
+    """ error message 
+        <type 'NoneType'>
+        <class 'bs4.element.Tag'>
+    """
+    for item in all_linked_pins:
+        if item.a == None:
+            continue
+        else:
+            pin_source.append(item.a.contents[0])
+    """ sample pin_source
+        [u'thefancy.com', u'raspberryandred.blogspot.com', 
+        u'rstyle.me', u'25.media.tumblr.com']
+    """    
+    return pin_source
 
 def count_source(pin_source_list):
 ### feed all sources and generate a list of tuples with (count, domain)
-	output = defaultdict(lambda: 0)
-	for source in pin_source_list:
-		try:
-			domain = source.split(".")[-2]
-			output[domain] += 1
-		except IndexError:
-			pass
-	
-	source_count = sorted(output.iteritems(),key=lambda (k,v): v,reverse=True)
-	# print "source_count", sorted(output.iteritems(),key=lambda (k,v): v,reverse=True)
-	return output
+    output = defaultdict(lambda: 0)
+    for source in pin_source_list:
+        try:
+            domain = source.split(".")[-2]
+            output[domain] += 1
+        except IndexError:
+            pass
+    return output
 
 def connect_db():
-	connect_string = "mongodb://pinterestalgo:pinterest2012@ds035617.mongolab.com:35617/pinterest"
-	# mongodb://%s:%s@%s:%d/%s" % \
- #            (user, password, host, port, db_name)
-	c = pymongo.connection.Connection(connect_string)
-	return c['pinterest']
+    connect_string = "mongodb://X:X@X.mongolab.com:X/X"
+    # mongodb://%s:%s@%s:%d/%s" % \
+    #    (user, password, host, port, db_name)
+    c = pymongo.connection.Connection(connect_string)
+    return c['pinterest']
 
 def main():
-	global db
-	db = connect_db()
-	# db = db['pinterest'] #get collection named pinterest
-	t = open("/Users/honeysnow/Desktop/python/beautifulsoup/datafiles/toppinners2.txt")
-	for line in t.readlines():
-		username = line.strip()
-		print username
-	# username = "michellelsun"
-		board_url_list = []	
-		board_list = find_board_list(username)
-		renamed_list = transform_boardname(board_list)
-		# print "BOARD LIST", renamed_list
-		""" BOARD LIST / renamed_list 
-			[u'fitness', u'beauty', u'home-sweet-home', u'words', u'sweetness',
-	 		u'wanderlust', u'fooodism', u'infographics', u'items', u'nature', 
-	 		u'love-in-the-bay', u'web-design', u'books ', u'demo-videos']
-		"""
-		for board in renamed_list: 
-			# print find_board_url(username,board)
-			if find_board_url(username, board) == None: 
-				continue
-			if len(find_board_url(username, board)) == 0:
-				continue
-			else:
-				board_url_list.extend(find_board_url(username, board))
-		# print "BOARD URL LIST", board_url_list
-		"""BOARD URL LIST / board_url_list 
-			[u'http://pinterest.com/michellelsun/fitness/', u'http://pinterest.com/michellelsun/beauty/', u'http://pinterest.com/michellelsun/beauty/?page=2', 
-			u'http://pinterest.com/michellelsun/beauty/?page=3', 
-			u'http://pinterest.com/michellelsun/home-sweet-home/', u'http://pinterest.com/michellelsun/home-sweet-home/?page=2', u'http://pinterest.com/michellelsun/words/', u'http://pinterest.com/michellelsun/words/?page=2', 
-			u'http://pinterest.com/michellelsun/sweetness/', u'http://pinterest.com/michellelsun/wanderlust/', u'http://pinterest.com/michellelsun/fooodism/', u'http://pinterest.com/michellelsun/infographics/', 
-			u'http://pinterest.com/michellelsun/items/', u'http://pinterest.com/michellelsun/nature/', u'http://pinterest.com/michellelsun/love-in-the-bay/', u'http://pinterest.com/michellelsun/web-design/', 
-			u'http://pinterest.com/michellelsun/demo-videos/']
-		"""
-		pin_source_list = make_pin_source_list(board_url_list)
-		"""pin_source_list 
-			[u'yogurtyoga.tumblr.com', u'tumblr.com', u'loseweight-safe.com', 
-			u'danceisuniversal.tumblr.com', u'dare-to-be-healthy.tumblr.com',
-			u'fit-not-thin.tumblr.com', u'fitsugar.com', u'flickr.com', 
-			u'migas.tumblr.com', u'tumblr.com', u'tumblr.com', u'tumblr.com', 
-			u'theathenenoctua.tumblr.com', u'awelltraveledwoman.tumblr.com',
-			u'fit-toned4summer.tumblr.com', u'youtube.com', u'tumblr.com', 
-			u'sexydangerous.tumblr.com'...] 
-			total length : 371 out of 461 total pins
-		"""
-		output = count_source(pin_source_list)
-		# print output
-		output_dict = {}
-		output_dict['pins'] = output
-		output_dict['username'] = username
-		print output_dict
-	# create mongodb database
-		pinners = db.pinners
-		pinners.insert(output_dict) #insert 
+    global db
+    db = connect_db()
+    # db = db['pinterest'] #get collection named pinterest
+    t = open("/Users/honeysnow/Desktop/python/beautifulsoup/datafiles/toppinners2.txt")
+    for line in t.readlines():
+        username = line.strip()
+        print username
+        board_url_list = [] 
+        board_list = find_board_list(username)
+        renamed_list = transform_boardname(board_list)
+        # print "BOARD LIST", renamed_list
+        """ BOARD LIST / renamed_list 
+            [u'wanderlust', u'fooodism', u'infographics', u'items', u'nature', 
+            u'love-in-the-bay', u'web-design', u'books ', u'demo-videos']
+        """
+        for board in renamed_list: 
+            # print find_board_url(username,board)
+            if find_board_url(username, board) == None: 
+                continue
+            if len(find_board_url(username, board)) == 0:
+                continue
+            else:
+                board_url_list.extend(find_board_url(username, board))
+        # print "BOARD URL LIST", board_url_list
+        """BOARD URL LIST / board_url_list 
+            [u'http://pinterest.com/michellelsun/fitness/', 
+            u'http://pinterest.com/michellelsun/beauty/', 
+            u'http://pinterest.com/michellelsun/beauty/?page=2', 
+            u'http://pinterest.com/michellelsun/beauty/?page=3', ']
+        """
+        pin_source_list = make_pin_source_list(board_url_list)
+        """pin_source_list 
+            [u'yogurtyoga.tumblr.com', u'tumblr.com', u'loseweight-safe.com', 
+            u'danceisuniversal.tumblr.com', u'dare-to-be-healthy.tumblr.com',
+            u'fit-not-thin.tumblr.com', u'fitsugar.com', u'flickr.com', 
+            u'migas.tumblr.com', u'tumblr.com', u'tumblr.com', u'tumblr.com', 
+            u'theathenenoctua.tumblr.com', u'awelltraveledwoman.tumblr.com',
+            u'fit-toned4summer.tumblr.com', u'youtube.com', u'tumblr.com', 
+            u'sexydangerous.tumblr.com'...] 
+            total length : 371 out of 461 total pins
+        """
+        output = count_source(pin_source_list)
+        output_dict = {}
+        output_dict['pins'] = output
+        output_dict['username'] = username
+    
 
-""" code that was used to update mongodb
-	for p in db.pinners.find():
-	     keys = p.keys()
-	     if "username" in keys:
-	             continue
-	     possible_names = [ k for k in keys if k != "_id" ]
-	     name = possible_names[0]
-	     p['pins'] = p[name]
-	     p['username'] = name
-	     del p[name]
-	     db.pinners.update({"_id": p["_id"]}, p)
-"""
-
-# class Pin(object):
-# 	def __init__ (self, link):
-# 		soup = get_soup(link)
-# 		self.title = etsy_item_title(soup)
-# 		self.price = etsy_price(soup)
-# 		self.image_link = etsy_img_link(soup)
-# 		self.pdt_link = link
+        # create mongodb database
+        pinners = db.pinners
+        pinners.insert(output_dict) #insert 
 
 if __name__ == '__main__':
-	main()
+    main()
 
